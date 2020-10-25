83c8fa76d85c1f7a20521960477b9b92474ddc19
Ahmad Yaqdhan <42838410+Mednoob@users.noreply.github.com>
2020-10-25 14:01:36 +0900

    Update and rename test.js to template.js

--- #2/commands/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "test",
-  description: "testing command",
-  alias: [],
-  run: async (client, message, args) => {
-    client.createMessage(message.channel.id, "Testing!")
-  }
-}
